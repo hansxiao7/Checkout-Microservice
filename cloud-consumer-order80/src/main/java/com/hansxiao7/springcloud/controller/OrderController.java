21775f562ba3a0a4aa821e129dbfb97464505969
Han Xiao <hankxiao@amazon.com>
2021-12-22 11:11:59 -0800

    Ribbon Load Balancer Implementation

--- cloud-consumer-order80/src/main/java/com/hansxiao7/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/hansxiao7/springcloud/controller/OrderController.java
@@ -2,12 +2,20 @@ package com.hansxiao7.springcloud.controller;
 
 import com.hansxiao7.springcloud.entities.CommonResult;
 import com.hansxiao7.springcloud.entities.Payment;
+import com.hansxiao7.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,13 +25,43 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private LoadBalancer loadBalancer;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment){
-        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    public CommonResult<?> create(Payment payment){
+        ResponseEntity<CommonResult> entity =  restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "Operation fails");
+        }
     }
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult getPayment(@PathVariable Long id){
-        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        ResponseEntity<CommonResult> entity =  restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "Operation fails");
+        }
+    }
+
+    @GetMapping("/consumer/payment/lb")
+    public String getPaymentLB(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        if (instances == null || instances.size() <= 0) return null;
+
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+
+        return restTemplate.getForEntity(uri + "/payment/lb", String.class).getBody();
     }
 }
---
+++ cloud-consumer-order80/src/main/java/com/hansxiao7/springcloud/lb/LoadBalancer.java
@@ -0,0 +1,9 @@
+package com.hansxiao7.springcloud.lb;
+
+import org.springframework.cloud.client.ServiceInstance;
+
+import java.util.List;
+
+public interface LoadBalancer {
+    ServiceInstance instances(List<ServiceInstance> serviceInstances);
+}
